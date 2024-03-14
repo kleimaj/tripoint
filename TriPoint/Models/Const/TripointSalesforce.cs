e3b3689fc9c7bf8b759b3f699eaf0106d9255eb4
Kyle Paulson <kpaulson@teamdms.com>
2024-03-13 17:57:07 -0700

    update to add salesforce crm

---
+++ TriPoint/Models/Const/TripointSalesforce.cs
@@ -0,0 +1,9 @@
+﻿namespace TriPoint.Models.Const;
+
+public static class TripointSalesforce {
+        internal static string grant_type = "client_credentials";
+        internal static string client_id = "3MVG9bYGb9rFSjxTzbHNydIGRr0ZYGn39ovzxAPvt..SCu_pdIZYW0D1XFaXsZgBhOViRloMDd8GFl23A33L3";
+        internal static string client_secret = "625C8CA55784221A4486FE6DD3B68BB5030B4E3D39BECD31F2C13B60BA717B44";
+        internal static string base_url = "https://tpl2.my.salesforce.com/services/apexrest/nu_dse/CreateProspect";
+        internal static string token_url = "https://tpl2.my.salesforce.com/services/oauth2/token";
+}
---
+++ TriPoint/Models/Tpl2Salesforce.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+
+namespace TriPoint.Models;
+
+public class Tpl2Salesforce {
+    public  class TripointProspects
+    {
+        [JsonProperty("prospects")]
+        public required Prospect[] Prospects { get; set; }
+    }
+
+    public  class Prospect
+    {
+        [JsonProperty("offer_code")]
+        public required string OfferCode { get; set; }
+
+        [JsonProperty("first_name")]
+        public required string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public required string LastName { get; set; }
+
+        [JsonProperty("address")]
+        public string? Address { get; set; }
+
+        [JsonProperty("city")]
+        public string? City { get; set; }
+
+        [JsonProperty("state")]
+        public string? State { get; set; }
+
+        [JsonProperty("zip_code")]
+        public string? ZipCode { get; set; }
+
+        [JsonProperty("home_phone")]
+        public required string HomePhone { get; set; }
+
+        [JsonProperty("cell_phone")]
+        public required string CellPhone { get; set; }
+
+        [JsonProperty("email")]
+        public required string Email { get; set; }
+
+        [JsonProperty("loan_amount")]
+        public required string LoanAmount { get; set; }
+        
+        [JsonProperty("lead_source")]
+        public required string Team { get; set; }
+    }
+    public partial class TripointSalesforceResponse
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+
+        [JsonProperty("instance_url")]
+        public Uri InstanceUrl { get; set; }
+
+        [JsonProperty("id")]
+        public Uri Id { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("issued_at")]
+        public string IssuedAt { get; set; }
+    }
+}
