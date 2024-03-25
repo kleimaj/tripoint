75f2e6ff393eb3dd1399f77d8226746d85800a00
jkleiman <jkleiman@teamdms.com>
2024-03-25 13:32:09 -0700

    adds calc

---
+++ TriPoint/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TriPoint.Models.Jsn;
+
+namespace TriPoint.Controllers;
+
+public class CalculatorController : Controller {
+    private readonly ILogger<FaqController> _logger;
+
+    public CalculatorController(ILogger<FaqController> logger) {
+        _logger = logger;
+    }
+
+    [HttpGet("calculator")]
+    public IActionResult Index() {
+        return View();
+    }
+
+    [HttpGet]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+        _logger.LogError("{HttpContextTraceIdentifier}", Activity.Current?.Id ?? HttpContext.TraceIdentifier);
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
